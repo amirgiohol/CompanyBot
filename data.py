8a7b7f90961349d6eae1a0befb33768d008ab94a
AmirsamDerakhshan <amirsamderakhshan@gmail.com>
2025-12-14 00:59:52 +0330

    add data module

---
+++ data.py
@@ -0,0 +1,24 @@
+courses = {
+    "Python": [
+        {
+            "id": 1,
+            "name": "Python مقدماتی",
+            "price": 10,
+            "description": "آموزش پایه پایتون"
+        },
+        {
+            "id": 2,
+            "name": "Python پیشرفته",
+            "price": 20,
+            "description": "پایتون حرفه‌ای"
+        }
+    ],
+    "Web": [
+        {
+            "id": 1,
+            "name": "HTML & CSS",
+            "price": 8,
+            "description": "طراحی سایت"
+        }
+    ]
+}
