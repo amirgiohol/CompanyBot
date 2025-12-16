ecbeac186813607a275d15b3945b33745f910868
AmirsamDerakhshan <amirsamderakhshan@gmail.com>
2025-12-17 00:37:55 +0330

    add data module

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -1,11 +1,11 @@
 from aiogram import Router
 from aiogram.types import Message
+from aiogram.filters import Command
 from app.keyboards import main_menu
-
 
 router = Router()
 
-@router.message(commands=["start"])
+@router.message(Command("start"))
 async def start_handler(message: Message):
     await message.answer(
         "به سامانه آموزشی برنامه‌نویسی خوش آمدید.\n"
