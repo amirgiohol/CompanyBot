bdc692e0de0019b8e62f49bf8f3bfc2f6693ef2d
AmirsamDerakhshan <amirsamderakhshan@gmail.com>
2025-12-17 00:45:01 +0330

    add data module

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("8530548516:AAGolHePa16jWsDHxew4nibN9v0NW6YHFSs")
 DATABASE_URL = os.getenv("DATABASE_URL")
 ADMIN_IDS = [123456789]  # آیدی عددی خودت
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
