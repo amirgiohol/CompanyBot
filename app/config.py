ba953ff6fd82a2c1d100ab369d6f6540c22d401b
AmirsamDerakhshan <amirsamderakhshan@gmail.com>
2025-12-17 00:50:55 +0330

    add data module

--- app/config.py
+++ app/config.py
@@ -2,5 +2,8 @@ import os
 
 BOT_TOKEN = os.getenv("8530548516:AAGolHePa16jWsDHxew4nibN9v0NW6YHFSs")
 DATABASE_URL = os.getenv("DATABASE_URL")
-ADMIN_IDS = [123456789]  # آیدی عددی خودت
+ADMIN_IDS = [123456789]
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+if BOT_TOKEN is None:
+    raise RuntimeError("BOT_TOKEN is not set!")
