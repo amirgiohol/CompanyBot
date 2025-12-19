4561d2386464aa233c34baebcfc7dcadc9709b90
AmirsamDerakhshan <amirsamderakhshan@gmail.com>
2025-12-19 17:41:29 +0330

    add data module

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from app.config import BOT_TOKEN
+from app.config import get_bot_token
 from app.handlers import start
 
 async def main():
+    BOT_TOKEN = get_bot_token()
     bot = Bot(token=BOT_TOKEN)
+
     dp = Dispatcher()
     dp.include_router(start.router)
+    
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
