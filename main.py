f99f073685d18dbbc89f5866eb37a50343e6a57d
AmirsamDerakhshan <amirsamderakhshan@gmail.com>
2025-12-19 18:04:09 +0330

    add data module

--- main.py
+++ main.py
@@ -4,12 +4,9 @@ from app.config import get_bot_token
 from app.handlers import start
 
 async def main():
-    BOT_TOKEN = get_bot_token()
-    bot = Bot(token=BOT_TOKEN)
-
+    bot = Bot(token=get_bot_token())
     dp = Dispatcher()
     dp.include_router(start.router)
-    
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
